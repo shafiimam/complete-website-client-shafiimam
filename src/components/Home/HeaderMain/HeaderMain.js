1e50db2e3c8b26390544b42cf1e961c696f76202
shafiimam <shafi15-8072@diu.edu.bd>
2021-04-18 18:08:04 +0600

    final commit

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -17,7 +17,7 @@ const HeaderMain = () => {
     return (
         <main className="row d-flex align-items-center p-2" style={customStyle}>
             <div className="col-md-4 offset-md-1  animate__animated animate__fadeInLeftBig animate__slower">
-                <h1  className="text-success" style={{ fontSize: 48, fontWeight: 'bold' }}>Let’s Grow Your <br /> Brand To The <br /> Next Level</h1>
+                <h1  className="text-success" style={{ fontSize: 48, fontWeight: 'bold' }}>Hire The <br /> Best Freelancers <br /> For Best Service</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta consectetur voluptate at autem. Dolorem recusandae architecto nam provident! Repellendus inventore ipsa minima amet perferendis rerum dolor quis.</p>
                 <button className="btn btn-dark" onClick={handleClick}>Hire us</button>
             </div>
